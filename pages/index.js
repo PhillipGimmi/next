f264e8a6c8b6ef1478df8c8c619bbefc625edc97
PhillipGimmi <104858994+PhillipGimmi@users.noreply.github.com>
2022-08-28 05:16:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import React, { useEffect, useState } from 'react';
 import Deck from '../components';
+function ClientOnly(props) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted ? props.children : null;
+}
 
 export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
-      <Deck />
+      <ClientOnly>
+        <Deck />
+      </ClientOnly>
     </div>
   );
 }
